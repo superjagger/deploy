2c2bc2755c632c5024f9068ed60ac739d61879be
superjagger <31763819+superjagger@users.noreply.github.com>
2024-03-30 15:02:57 +0800

    Update deploy_rust.sh

--- deploy_rust.sh
+++ deploy_rust.sh
@@ -6,7 +6,19 @@ source $HOME/.bash_profile
 
 if ! command -v rustc &>/dev/null; then
     echo "rust 未安装"
-
+    # 安装 rust
+    cd $HOME
+    curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf > rustup.sh
+    bash rustup.sh -y
+    rm -rf rustup.sh
+    # 写入环境变量，永久生效
+    echo 'export PATH=$PATH:$HOME/.cargo/env' >>$HOME/.bash_profile
+    source $HOME/.bash_profile
+    
+    # 查看安装后版本
+    rustc --version
+    cargo --version
+    
 else
     echo "rust 已经安装"
 fi
