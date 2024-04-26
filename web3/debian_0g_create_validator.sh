c48d791c65f59ae60a997c1bff861d505e4e375c
superjagger <31763819+superjagger@users.noreply.github.com>
2024-04-26 21:04:08 +0800

    Update debian_0g_create_validator.sh

--- web3/debian_0g_create_validator.sh
+++ web3/debian_0g_create_validator.sh
@@ -3,11 +3,13 @@
 # 系统: Debian
 # 项目: 0g
 # 备注: 创建验证者，必须领过水的，同时区块高度能够查到领水钱包，不然无法创建验证者
-# 执行: curl -sSL https://raw.githubusercontent.com/superjagger/deploy/main/web3/debian_0g_create_validator.sh | bash -s -- "钱包地址" "钱包秘钥" "助记词"
+# 执行: curl -sSL https://raw.githubusercontent.com/superjagger/deploy/main/web3/debian_0g_create_validator.sh | bash -s -- "IP" "服务器密码" "钱包地址" "钱包秘钥" "助记词"
 
-address=$1
-private_key=$2
-mnemonic=$3
+ip=$1
+password=$2
+address=$3
+private_key=$4
+mnemonic=$5
 if which evmosd > /dev/null 2>&1; then
     echo "evmosd 已经安装"
 else
@@ -15,11 +17,12 @@ else
     curl -sSL https://raw.githubusercontent.com/superjagger/deploy/main/web3/debian_deploy_0g.sh | bash -s -- "${address}" "BwZpAouIfwNWkzw"
 fi
 
+sudo apt-get install -y expect
 
 # 下载 exp 安装 导入助记词脚本
 curl -O https://raw.githubusercontent.com/superjagger/deploy/main/web3/debian_0g_recover_mnemonic.exp
 
 # 使用导入助记词
-expect debian_0g_recover_mnemonic.exp "${address}" "${private_key}" "${mnemonic}"
+expect debian_0g_recover_mnemonic.exp "${ip}" "${password}" "${address}" "${private_key}" "${mnemonic}"
 
 rm debian_0g_recover_mnemonic.exp
