d0ad6fe791164bba2b930a2fbcf21ef957aaa9a4
superjagger <31763819+superjagger@users.noreply.github.com>
2024-04-26 15:26:53 +0800

    Update ubuntu_deploy_taiko.sh

--- web3/ubuntu_deploy_taiko.sh
+++ web3/ubuntu_deploy_taiko.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-# 部署taiko验证者节点，命令行: curl -sSL https://raw.githubusercontent.com/superjagger/deploy/main/web3/ubuntu_deploy_taiko.sh | bash -s -- "私钥" "Ethereum Sepolia HTTP RPC连接" "Ethereum Sepolia WS RPC连接"
+# 部署taiko验证者节点，命令行: curl -sSL https://raw.githubusercontent.com/superjagger/deploy/main/web3/ubuntu_deploy_taiko.sh | bash -s -- "私钥" "Ethereum Holskey HTTP RPC连接" "Ethereum Holskey WS RPC连接" "Beacon Holskey RPC连接"
 
 echo "开始部署taiko验证者节点"
 private_key=$1
 http_rpc=$2
 ws_rpc=$3
+beacon_rpc=$4
 
 echo "private_key=${private_key}"
 echo "http_rpc=${http_rpc}"
 echo "ws_rpc=${ws_rpc}"
+echo "ws_rpc=${beacon_rpc}"
 
 if [ -z "$private_key" ] || [ -z "$http_rpc" ] || [ -z "$ws_rpc" ]; then
     echo "缺少参数"
@@ -36,6 +38,7 @@ cd simple-taiko-node
 cp .env.sample .env
 
 sed -i -e "s#^L1_ENDPOINT_HTTP=.*#L1_ENDPOINT_HTTP=$http_rpc#; s#^L1_ENDPOINT_WS=.*#L1_ENDPOINT_WS=$ws_rpc#; s#^ENABLE_PROVER=false.*#ENABLE_PROVER=true#; s#^L1_PROVER_PRIVATE_KEY=.*#L1_PROVER_PRIVATE_KEY=$private_key#" .env
+sed -i "s|L1_BEACON_HTTP=.*|L1_BEACON_HTTP=${beacon_rpc}|" .env
 echo "停止容器"
 docker compose down
 echo "启动容器"
