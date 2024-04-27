5fd2916df672561471f713e6deb8eea2cf6a55f4
superjagger <31763819+superjagger@users.noreply.github.com>
2024-04-27 16:46:45 +0800

    Update debian_deploy_docker.sh

--- debian_deploy_docker.sh
+++ debian_deploy_docker.sh
@@ -9,6 +9,7 @@ if which docker > /dev/null 2>&1; then
     echo "Docker 已经安装"
 else
     echo "Docker 未安装"
+    echo "添加 Docker GPG key "
     # Add Docker's official GPG key:
     sudo apt-get update
     sudo apt-get install ca-certificates curl
@@ -16,13 +17,16 @@ else
     sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
     sudo chmod a+r /etc/apt/keyrings/docker.asc
 
+    echo "添加 Docker 源 "
     # Add the repository to Apt sources:
     echo \
         "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
         sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+    echo "更新 apt-get"
     sudo apt-get update
 
+    echo "安装 Docker"
     # 安装
     sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
