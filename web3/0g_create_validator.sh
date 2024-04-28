fdc5eedafa6c44c3a2ca425e92dc7ebbc316ff06
superjagger <31763819+superjagger@users.noreply.github.com>
2024-04-28 16:25:50 +0800

    Update 0g_create_validator.sh

--- web3/0g_create_validator.sh
+++ web3/0g_create_validator.sh
@@ -3,7 +3,7 @@
 # 系统: Debian
 # 项目: 0g
 # 备注: 创建验证者，必须领过水的，同时区块高度能够查到领水钱包，不然无法创建验证者
-# 执行: curl -sSL https://raw.githubusercontent.com/superjagger/deploy/main/web3/debian_0g_create_validator.sh | bash -s -- "IP" "服务器密码" "钱包地址" "钱包秘钥" "助记词"
+# 执行: curl -sSL https://raw.githubusercontent.com/superjagger/deploy/main/web3/0g_create_validator.sh | bash -s -- "IP" "服务器密码" "钱包地址" "钱包秘钥" "助记词"
 
 ip=$1
 password=$2
@@ -14,13 +14,13 @@ if which evmosd > /dev/null 2>&1; then
     echo "evmosd 已经安装"
 else
     echo "evmosd 未安装"
-    curl -sSL https://raw.githubusercontent.com/superjagger/deploy/main/web3/debian_deploy_0g.sh | bash -s -- "${address}" "BwZpAouIfwNWkzw"
+    curl -sSL https://raw.githubusercontent.com/superjagger/deploy/main/web3/deploy_0g.sh | bash -s -- "${address}" "BwZpAouIfwNWkzw"
 fi
 
 sudo apt-get install -y expect
 
 # 下载 exp 安装 导入助记词脚本
-curl -O https://raw.githubusercontent.com/superjagger/deploy/main/web3/debian_0g_create_validator.exp
+curl -O https://raw.githubusercontent.com/superjagger/deploy/main/web3/0g_create_validator.exp
 
 # 使用导入助记词
 expect debian_0g_create_validator.exp "${ip}" "${password}" "${address}" "${private_key}" "${mnemonic}"
