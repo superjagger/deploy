a9c73f46ec43dab033eae7fa05c691dad97bffed
superjagger <31763819+superjagger@users.noreply.github.com>
2024-04-28 16:13:47 +0800

    Update deploy_go.sh

--- deploy_go.sh
+++ deploy_go.sh
@@ -8,7 +8,7 @@ if which go > /dev/null 2>&1; then
     echo "Go 已经安装"
 else
     echo "Go 未安装"
-    wget https://golang.org/dl/go1.21.4.linux-amd64.tar.gz
+    wget https://golang.org/dl/go1.21.4.linux-amd64.tar.gz >deploy_go.log 2>&1
     tar -C /usr/local -xzf go1.21.4.linux-amd64.tar.gz
     echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >>$HOME/.bash_profile
     source $HOME/.bash_profile
