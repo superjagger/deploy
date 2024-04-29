1b8ca1d7bd81a263e8c5bdf502be1e788d8b778e
superjagger <31763819+superjagger@users.noreply.github.com>
2024-04-30 02:04:44 +0800

    Update and rename escape_for_expect.sh to escape.sh

--- escape.sh
+++ escape.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
+# 不同的工具集需要的字符串转义
 # 以下代码加入到bash脚本就可以导入函数，引用函数
 # curl -O https://raw.githubusercontent.com/superjagger/deploy/main/escape_for_expect.sh && source escape_for_expect.sh && rm ./escape_for_expect.sh
 
 # 转义 expect 中的特殊字符
 function escape_for_expect() {
     input="$1"
-    # 使用 sed 替换特定字符为它们的转义形式
-    # 注意：这里只列出了一些常见的特殊字符，你可能需要添加更多
     adapted_password=$(
         echo $input |
             sed 's,\\,\\\\,g' |
@@ -20,11 +19,9 @@ function escape_for_expect() {
     echo "$adapted_password"
 }
 
-# 转义 expect 中的特殊字符
+# 转义 sshpass ssh 命令行的特殊字符
 function escape_for_sshpass() {
     input="$1"
-    # 使用 sed 替换特定字符为它们的转义形式
-    # 注意：这里只列出了一些常见的特殊字符，你可能需要添加更多
     adapted_password=$(
         echo $input |
             sed 's,\\,\\\\,g' |
