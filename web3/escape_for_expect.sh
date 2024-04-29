0463d23a79650113a27b7bf069d2f5cf524898e1
superjagger <31763819+superjagger@users.noreply.github.com>
2024-04-30 00:16:38 +0800

    Create escape_for_expect.sh

---
+++ web3/escape_for_expect.sh
@@ -0,0 +1,15 @@
+
+
+# 下载本地然后引用函数 
+# wget -O temp_script.sh https://raw.githubusercontent.com/superjagger/deploy/main/escape_for_expect.sh 
+# ./temp_script.sh
+# rm ./temp_script.sh
+
+# 转义 expect 中的特殊字符  
+function escape_for_expect() {  
+    local input="$1"  
+    # 使用 sed 替换特定字符为它们的转义形式  
+    # 注意：这里只列出了一些常见的特殊字符，你可能需要添加更多  
+    local escaped=$(echo "$input" | sed 's/\\/\\\\/g;s/\$/\\\$/g;s/\{/\\\{/g;s/\}/\\\}/g;s/\[/\\\[/g;s/\]/\\\]/g;s/;/\\;/g')  
+    echo "$escaped"  
+}  
