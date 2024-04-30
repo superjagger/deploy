820ae12cb447c732fca5ad535d9f26c9bdce8647
superjagger <31763819+superjagger@users.noreply.github.com>
2024-04-30 20:33:38 +0800

    Create deploy_foundry.sh

---
+++ deploy_foundry.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source $HOME/.bash_profile
+
+# foundry 安装脚本，执行: curl -sSL https://raw.githubusercontent.com/superjagger/deploy/main/deploy_foundry.sh | bash
+
+if which forge > /dev/null 2>&1; then
+    echo "foundry 已经安装"
+else
+    echo "foundry 未安装"
+    curl -L https://foundry.paradigm.xyz | bash
+    source /root/.bashrc
+    foundryup
+fi
