3045283db20e92cd273d9410f64f7bc97eb3ce95
superjagger <31763819+superjagger@users.noreply.github.com>
2024-05-27 04:05:43 +0800

    Update deploy_tanssi.sh

--- web3/deploy_tanssi.sh
+++ web3/deploy_tanssi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# curl -sSL https://raw.githubusercontent.com/superjagger/deploy/main/web3/deploy_tanssi.sh | bash -s -- "节点名称" "生成者名称" "中继者名称"
+# curl -sSL https://raw.githubusercontent.com/superjagger/deploy/main/web3/deploy_tanssi.sh | bash -s -- "节点名称" "生成者名称" "中继者名称" "是否重装，1 表示重装节点"
 
 # 节点名称
 node_name=$1
@@ -16,15 +16,39 @@ if [ -z "$relay_node_name" ]; then
     exit 1
 fi
 
+clear=$4
+if [ -z "$clear" ]; then
+    clear=0
+fi
+
 tanssi_dir=$HOME/tanssi_dir
 tanssi_data=$tanssi_dir/data
 
 mkdir -p $tanssi_dir
 mkdir -p $tanssi_data
 
-# 暂停并删除容器
-docker stop tanssi
-docker rm tanssi
+if [ $clear -eq 1 ]; then
+    echo "本次脚本会删除原有数据，如果不想删除及时退出脚本"
+    sleep 1
+    echo "3"
+    sleep 1
+    echo "2"
+    sleep 1
+    echo "1"
+    # 暂停并删除容器
+    docker stop tanssi
+    docker rm tanssi
+fi
+
+# 安装docker
+curl -sSL https://raw.githubusercontent.com/superjagger/deploy/main/deploy_docker.sh | bash
+
+# 检查是否有部署的tanssi容器
+if [[ -n $(docker ps -a -q -f "name=tanssi") ]]; then
+    echo "tanssi节点已经部署，只进行启动服务"
+    docker start tanssi
+    exit
+fi
 
 # 启动容器
 docker run -d --network="host" --name tanssi -v "$tanssi_data:/data" \
@@ -53,5 +77,5 @@ docker run -d --network="host" --name tanssi -v "$tanssi_data:/data" \
     --telemetry-url='wss://telemetry.polkadot.io/submit/ 0' \
     --database paritydb
 
-sleep 5 
+sleep 5
 docker logs -n 10 tanssi
