738f1614f19c7d89351033f55a684a7d96b71da1
superjagger <31763819+superjagger@users.noreply.github.com>
2024-06-28 22:31:08 +0800

    Create deploy_process_exporter.sh

---
+++ prometheus/deploy_process_exporter.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+prometheus_dir=$HOME/prometheus
+process_dir=$prometheus_dir/process-exporter
+mkdir -p $prometheus_dir
+mkdir -p $process_dir
+
+# 安装docker
+curl -sSL https://raw.githubusercontent.com/superjagger/deploy/main/deploy_docker.sh | bash
+
+cd $process_dir
+cat >$process_dir/config.yml<<EOF
+process_names:
+  - name: "{{.Matches}}"
+    cmdline:
+    - '0gchaind'
+  - name: "{{.Matches}}"
+    cmdline:
+    - 'zgs_node'
+  - name: "{{.Matches}}"
+    cmdline:
+    - 'artelad'
+EOF
+docker run -d -it -p 9256:9256 --privileged -v /proc:/host/proc -v $process_dir:/config --name=process-exporter --restart=always ncabatoff/process-exporter --procfs /host/proc -config.path /config/config.yml
+docker restart
