3f36d2a70290fa4722d8c00cfeb941e34cea6094
superjagger <31763819+superjagger@users.noreply.github.com>
2024-10-16 00:06:54 +0800

    Create update_quili_2.0.sh

---
+++ web3/update_quili_2.0.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# 文件地址
+quili_dir=$HOME/quili_dir
+run_node_sh=$quili_dir/run_ceremonyclient_node.sh
+service_name=ceremonyclient
+
+# 更新dns
+echo "更新dns"
+sh -c 'echo "nameserver 8.8.8.8\nnameserver 8.8.4.4" > /etc/resolv.conf'
+
+# 安装go
+go_version=1.20.14
+curl -sSL https://raw.githubusercontent.com/superjagger/deploy/main/deploy_go_version.sh | bash -s -- $go_version
+go_dir=/usr/local/go_${go_version}
+export PATH=$PATH:${go_dir}/go/bin:$HOME/go/bin
+
+# 下载最新分支
+cd $quili_dir
+rm -rf ceremonyclient
+git clone -b release-cdn https://git.dadunode.com/smeb_y/ceremonyclient.git
+
+#手动下载更新quil node二进制文件
+cd $quili_dir/ceremonyclient/node/
+RELEASE_FILES_URL="https://releases.quilibrium.com/release"
+OS_ARCH=linux-amd64
+RELEASE_FILES=$(curl -s $RELEASE_FILES_URL | grep -oE "node-[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+-${OS_ARCH}(\.dgst)?(\.sig\.[0-9]+)?")
+for file in $RELEASE_FILES; do
+    wget "https://releases.quilibrium.com/$file"
+done
+chmod +x node-2*
+
+
+#手动下载更新quil qclient二进制文件
+cd $quili_dir/ceremonyclient/node/
+RELEASE_FILES_URL="https://releases.quilibrium.com/qclient-release"
+OS_ARCH=linux-amd64
+RELEASE_FILES=$(curl -s $RELEASE_FILES_URL | grep -oE "qclient-[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+-${OS_ARCH}(\.dgst)?(\.sig\.[0-9]+)?")
+for file in $RELEASE_FILES; do
+    wget "https://releases.quilibrium.com/$file"
+done
+chmod +x qclient-2*
+
+#查询本机的Effective seniority score
+cd $quili_dir/ceremonyclient/node/
+echo "查询本机的Effective seniority score"
+./qclient-2.0.0.2-linux-amd64 config prover merge --dry-run .config/ .config/
+
+# 编写节点启动脚本
+cat >$run_node_sh <<EOF
+go_dir=/usr/local/go_${go_version}
+export PATH=\$PATH:\${go_dir}/go/bin:\$HOME/go/bin
+cd $quili_dir/ceremonyclient/node
+./node-2.0.0.3-linux-amd64
+EOF
